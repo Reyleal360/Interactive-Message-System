b28e13fbfd339ad3f0a5f46009d16bdb437b7757
Reinaldo Andres Leal Brochero <rexleal360@gmail.com>
2025-06-20 17:02:02 -0500

    feat:bug with updated age

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,8 @@ function calculo(){
     let name = document.getElementById("name").value;
     let age  = parseInt(document.getElementById("age").value);
     let resultElement = document.getElementById("result");
-
-    if( age <= 0 || isNaN(age) || !name ){
-        resultElement.innerText= "Please enter valid values.";
+    if (age <= 0 || isNaN(age) || !name || !esFloat(age)) {
+        resultElement.innerText = "Please enter valid values.";
         return;
     } else if(age>=18){
         resultElement.innerText= `Hi ${name}, you're of legal age. Get ready for great opportunities in the world of programming!`;
